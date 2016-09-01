583ded9d7ed1d69393d8db4b5ea89cf670e4f07f
warned <dwarne14@gmail.com>
2016-09-01 05:09:22 -0400

    updates

---
+++ Gravity/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Gravity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.tmrUpdate = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMouseY = new System.Windows.Forms.Label();
+            this.lblMouseX = new System.Windows.Forms.Label();
+            this.tmrMouseCoords = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblCountBad = new System.Windows.Forms.Label();
+            this.lblCountGood = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblRandX = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(90, 85);
+            this.pb1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(792, 1050);
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // tmrUpdate
+            // 
+            this.tmrUpdate.Interval = 10;
+            this.tmrUpdate.Tick += new System.EventHandler(this.tmrUpdate_Tick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.ForeColor = System.Drawing.Color.Purple;
+            this.label2.Location = new System.Drawing.Point(10, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X: ";
+            this.label2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // lblMouseY
+            // 
+            this.lblMouseY.AutoSize = true;
+            this.lblMouseY.BackColor = System.Drawing.Color.Black;
+            this.lblMouseY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblMouseY.ForeColor = System.Drawing.Color.Purple;
+            this.lblMouseY.Location = new System.Drawing.Point(56, 63);
+            this.lblMouseY.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblMouseY.Name = "lblMouseY";
+            this.lblMouseY.Size = new System.Drawing.Size(41, 44);
+            this.lblMouseY.TabIndex = 1;
+            this.lblMouseY.Text = "0";
+            this.lblMouseY.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // lblMouseX
+            // 
+            this.lblMouseX.AutoSize = true;
+            this.lblMouseX.BackColor = System.Drawing.Color.Black;
+            this.lblMouseX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblMouseX.ForeColor = System.Drawing.Color.Purple;
+            this.lblMouseX.Location = new System.Drawing.Point(56, 17);
+            this.lblMouseX.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblMouseX.Name = "lblMouseX";
+            this.lblMouseX.Size = new System.Drawing.Size(41, 44);
+            this.lblMouseX.TabIndex = 1;
+            this.lblMouseX.Text = "0";
+            this.lblMouseX.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // tmrMouseCoords
+            // 
+            this.tmrMouseCoords.Interval = 10;
+            this.tmrMouseCoords.Tick += new System.EventHandler(this.tmrMouseCoords_Tick);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.ForeColor = System.Drawing.Color.Purple;
+            this.label3.Location = new System.Drawing.Point(10, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 46);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "X: ";
+            this.label3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(834, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Good:";
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.ForeColor = System.Drawing.Color.Purple;
+            this.label4.Location = new System.Drawing.Point(834, 63);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 46);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Bad:";
+            this.label4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // lblCountBad
+            // 
+            this.lblCountBad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCountBad.AutoSize = true;
+            this.lblCountBad.BackColor = System.Drawing.Color.Black;
+            this.lblCountBad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblCountBad.ForeColor = System.Drawing.Color.Purple;
+            this.lblCountBad.Location = new System.Drawing.Point(948, 63);
+            this.lblCountBad.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCountBad.Name = "lblCountBad";
+            this.lblCountBad.Size = new System.Drawing.Size(41, 44);
+            this.lblCountBad.TabIndex = 1;
+            this.lblCountBad.Text = "0";
+            this.lblCountBad.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // lblCountGood
+            // 
+            this.lblCountGood.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCountGood.AutoSize = true;
+            this.lblCountGood.BackColor = System.Drawing.Color.Black;
+            this.lblCountGood.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblCountGood.ForeColor = System.Drawing.Color.Purple;
+            this.lblCountGood.Location = new System.Drawing.Point(948, 17);
+            this.lblCountGood.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCountGood.Name = "lblCountGood";
+            this.lblCountGood.Size = new System.Drawing.Size(41, 44);
+            this.lblCountGood.TabIndex = 1;
+            this.lblCountGood.Text = "0";
+            this.lblCountGood.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label5.ForeColor = System.Drawing.Color.Purple;
+            this.label5.Location = new System.Drawing.Point(834, 137);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 46);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Rx: ";
+            this.label5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb1_Click);
+            // 
+            // lblRandX
+            // 
+            this.lblRandX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblRandX.AutoSize = true;
+            this.lblRandX.BackColor = System.Drawing.Color.Black;
+            this.lblRandX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblRandX.ForeColor = System.Drawing.Color.Purple;
+            this.lblRandX.Location = new System.Drawing.Point(908, 137);
+            this.lblRandX.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblRandX.Name = "lblRandX";
+            this.lblRandX.Size = new System.Drawing.Size(41, 44);
+            this.lblRandX.TabIndex = 1;
+            this.lblRandX.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 1229);
+            this.Controls.Add(this.lblCountGood);
+            this.Controls.Add(this.lblMouseX);
+            this.Controls.Add(this.lblRandX);
+            this.Controls.Add(this.lblCountBad);
+            this.Controls.Add(this.lblMouseY);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pb1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.Timer tmrUpdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMouseY;
+        private System.Windows.Forms.Label lblMouseX;
+        private System.Windows.Forms.Timer tmrMouseCoords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCountBad;
+        private System.Windows.Forms.Label lblCountGood;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblRandX;
+    }
+}
+
---
+++ Gravity/Form1.cs
@@ -0,0 +1,364 @@
+﻿//TODO
+//1.Add last position so that point where ball touches ground can be calculated exactly
+//2.Change delete algorithm to add ball numbers to queue
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gravity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Start Structs & Lists
+        public struct Ball
+        {
+            public double[] fPos;
+            public double[] fVel;
+            public double[] fAcc;
+            public double fMass;
+            public double fDiam;
+            public Color cFill;
+            public Color cOutline;
+            public Boolean bDelete;
+            public Boolean bBadPos;
+            public Timer tmrBeforeDelete;
+            public int iExpired;
+            //Constructor
+            public Ball(double[] fPos, double[] fVel, double[] fAcc, double fMass, double fDiam, Color cFill, Color cOutline, Boolean bDelete, Boolean bBadPos, Timer tmrBeforeDelete, int iExpired)
+            {
+                this.fPos = fPos;
+                this.fVel = fVel;
+                this.fAcc = fAcc;
+                this.fMass = fMass;
+                this.fDiam = fDiam;
+                this.cFill = cFill;
+                this.cOutline = cOutline;
+                this.bDelete = bDelete;
+                this.bBadPos = bBadPos;
+                this.tmrBeforeDelete = tmrBeforeDelete;
+                this.iExpired = iExpired;
+            }
+        }
+        public struct Force
+        {
+            public double[] fTotal;
+            public double[] fNoise;
+            public double[] fRand;
+            public Force(double[] fTotal, double[] fNoise, double[] fRand)
+            {
+                this.fTotal = fTotal;
+                this.fNoise = fNoise;
+                this.fRand = fRand;
+            }
+        }
+        public struct Control
+        {
+            public DateTime dPLastUpdate;
+            public DateTime dVLastUpdate;
+
+            public Control(DateTime dPLastUpdate, DateTime dVLastUpdate)
+            {
+                this.dPLastUpdate = dPLastUpdate;
+                this.dVLastUpdate = dPLastUpdate;
+            }
+        }
+        public struct SysVars
+        {
+            public double fGravity;
+            public double iGround;
+            public int iGap;
+            public double fRandForce;
+            public int iHeader;
+            public SysVars(double fGravity, int iGround, int iGap, double fRandForce, int iHeader)
+            {
+                this.fGravity = fGravity;
+                this.iGround = iGround;
+                this.iGap = iGap;
+                this.fRandForce = fRandForce;
+                this.iHeader = iHeader;
+            }
+        }
+        //
+        public List<Ball> myBalls = new List<Ball>();
+        public SysVars mySysVars = new SysVars();
+        public List<Force> myForces = new List<Force>();
+        public List<Control> myControls = new List<Control>();
+        // End Structs & Lists
+
+        // Start Initialize
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pb1.Paint += new System.Windows.Forms.PaintEventHandler(this.pb1_Paint);
+            pb1.Dock = DockStyle.Fill;
+            pb1.BackColor = Color.Black;
+            InitializeSysVars();
+            tmrUpdate.Enabled = true;
+            tmrMouseCoords.Enabled = true;
+            UpdateRandomForce();
+        }
+        private void InitializeSysVars()
+        {
+            mySysVars.iGround = 30;
+            mySysVars.fGravity = 1;
+            mySysVars.iGap = 200;
+            mySysVars.iHeader = 100;
+        }
+        // End Initialize
+        
+        private void pb1_Click(object sender, EventArgs e)
+        {
+
+            Point p = this.PointToScreen(new Point(0, 0));
+            if (MousePosition.Y - p.Y < mySysVars.iHeader)
+            {
+                myBalls.Add(new Ball(new double[2] { MousePosition.X - p.X, MousePosition.Y - p.Y }, new double[2] { 0, 0 }, new double[2] { 0, 0 }, 1, 20, Color.Blue, Color.Blue, false, false, new Timer(), 0));
+                myBalls[myBalls.Count - 1].tmrBeforeDelete.Interval = 4000;
+                myBalls[myBalls.Count - 1].tmrBeforeDelete.Tick += new System.EventHandler(this.tmrBeforeDelete_Tick);
+                myForces.Add(new Force(new double[2] { 0, 0 }, new double[2] { 0, 0 }, new double[2] {mySysVars.fRandForce, 0 }));
+                UpdateForces(myForces[myForces.Count - 1]);
+                myControls.Add(new Control(System.DateTime.Now, System.DateTime.Now));
+            }
+        }
+     
+        // Start Handle Bad Balls
+        private void CheckIfPosIsValid(int i)
+        {
+            Ball ballTemp;
+            int iMaxEx;
+
+            ballTemp = myBalls[i];
+            if (!ballTemp.bBadPos)
+            {
+                // if Ball hits left side of screen
+                if ((ballTemp.fPos[0] - ballTemp.fDiam) < 0) 
+                {
+                    //b.fPos[0] = b.fDiam;
+                    ballTemp.cFill = Color.Red;
+                    HaltBall(ballTemp, myForces[i]);
+                    lblCountBad.Text = Convert.ToString(Convert.ToInt32(lblCountBad.Text) + 1);
+                    UpdateRandomForce();
+                    ballTemp.bBadPos = true;
+                }
+                // if Ball hits right side of screen
+                if (ballTemp.fPos[0] + ballTemp.fDiam > pb1.Width)
+                {
+                    //b.fPos[0] = pb1.Width - b.fDiam;
+                    ballTemp.cFill = Color.Red;
+                    HaltBall(ballTemp, myForces[i]);
+                    lblCountBad.Text = Convert.ToString(Convert.ToInt32(lblCountBad.Text) + 1);
+                    UpdateRandomForce();
+                    ballTemp.bBadPos = true;
+                }
+                // if ball hits ground level ground
+                if (ballTemp.fPos[1] + (ballTemp.fDiam / 2) > (pb1.Height - mySysVars.iGround))
+                {
+                    // if ball hits left side of ground
+                    if ((ballTemp.fPos[0] < ((pb1.Width - (mySysVars.iGap - ballTemp.fDiam)) / 2)))
+                    {
+                        //b.fPos[1] =  (pb1.Height - mySysVars.iGround)- (b.fDiam / 2);
+                        //b.fPos[0] = ((pb1.Width - (mySysVars.iGap - b.fDiam)) / 2);
+                        ballTemp.cFill = Color.Red;
+                        HaltBall(ballTemp, myForces[i]);
+                        lblCountBad.Text = Convert.ToString(Convert.ToInt32(lblCountBad.Text) + 1);
+                        UpdateRandomForce();
+                        ballTemp.bBadPos = true;
+                    }
+                    // if ball hits right side of ground
+                    if (ballTemp.fPos[0] > ((pb1.Width + mySysVars.iGap - ballTemp.fDiam) / 2))
+                    {
+                        //b.fPos[1] =  (pb1.Height - mySysVars.iGround)- (b.fDiam / 2);
+                        //b.fPos[0] = ((pb1.Width + mySysVars.iGap - b.fDiam) / 2);
+                        ballTemp.cFill = Color.Red;
+                        HaltBall(ballTemp, myForces[i]);
+                        lblCountBad.Text = Convert.ToString(Convert.ToInt32(lblCountBad.Text) + 1);
+                        UpdateRandomForce();
+                        ballTemp.bBadPos = true;
+                    }
+                }
+                if(ballTemp.fPos[1] > (pb1.Height + (ballTemp.fDiam/2)))
+                {
+                    lblCountGood.Text = Convert.ToString(Convert.ToInt32(lblCountGood.Text) + 1);
+                    UpdateRandomForce();
+                    ballTemp.bDelete = true;
+                }
+            }
+            if (ballTemp.bBadPos)
+            {
+                iMaxEx = 0;
+                for (int i2 = 0; i2 < myBalls.Count; i2++)
+                {
+                    if (myBalls[i2].iExpired > iMaxEx)
+                    {
+                        iMaxEx = myBalls[i2].iExpired;
+                    }
+                }
+                if (ballTemp.iExpired == 0)
+                {
+                    ballTemp.iExpired = iMaxEx + 1;
+                    ballTemp.tmrBeforeDelete.Enabled = true;
+                }
+            }
+            myBalls[i] = ballTemp;
+        }
+        private void HaltBall(Ball b, Force f)
+        {
+            for (int i = 0; i < b.fAcc.Length; i++)
+            {
+                b.fAcc[i] = 0;
+                b.fVel[i] = 0;
+                f.fTotal[i] = Convert.ToDouble(0);
+            }
+        }
+        private void PurgeBalls()
+        {
+            List<Ball> lstTempBalls = new List<Ball>();
+            List<Force> lstTempForces = new List<Force>();
+            List<Control> lstTempControls = new List<Control>();
+
+            for (int i = 0; i < myBalls.Count; i++)
+            {
+                if (myBalls[i].bDelete == false)
+                {
+                    lstTempBalls.Add(myBalls[i]);
+                    lstTempControls.Add(myControls[i]);
+                    lstTempForces.Add(myForces[i]);
+                }
+            }
+            myBalls = lstTempBalls;
+            myControls = lstTempControls;
+            myForces = lstTempForces;
+        }
+        private void tmrBeforeDelete_Tick(Object sender, EventArgs e)
+        {
+            Ball ballTemp;
+            for (int i = 0; i < myBalls.Count; i++)
+            {
+                if (myBalls[i].iExpired == 1)
+                {
+                    ballTemp = myBalls[i];
+                    ballTemp.bDelete = true;
+                    ballTemp.tmrBeforeDelete.Enabled = false;
+                    myBalls[i] = ballTemp;
+                }
+                else if (myBalls[i].iExpired > 1)
+                {
+                    ballTemp = myBalls[i];
+                    ballTemp.iExpired -= 1;
+                    myBalls[i] = ballTemp;
+                }
+            }
+        }
+        // End Handle Bad Balls
+        
+        private void pb1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            //Pen p = System.Drawing.Pens.Blue;
+            SolidBrush b = new SolidBrush(Color.White);
+
+            // Create a local version of the graphics object for the PictureBox.
+            Graphics g = e.Graphics;
+            b = new SolidBrush(Color.Green);
+            g.FillRectangle(b, new Rectangle(0, (int)(pb1.Height - mySysVars.iGround), (int)((pb1.Width) / 2 - (mySysVars.iGap / 2)), (int)mySysVars.iGround));
+            //g.FillRectangle(b, new Rectangle((int)((pb1.Width) / 2 - 50), (int)(pb1.Height - mySysVars.iGround), 100, (int)mySysVars.iGround));
+            g.FillRectangle(b, new Rectangle((int)((pb1.Width) / 2 + (mySysVars.iGap / 2)), (int)(pb1.Height - mySysVars.iGround), (int)((pb1.Width) / 2 - (mySysVars.iGap / 2)), (int)mySysVars.iGround));
+
+            for (int i = 0; i < myBalls.Count; i++)
+            {
+                b = new SolidBrush(myBalls[i].cFill);
+                //g.DrawEllipse(p, (int)((ball.fPos[0]) - ((ball.fDiam) / 2)), (int)((ball.fPos[1]) - ((ball.fDiam) / 2)), (int)ball.fDiam, (int)ball.fDiam);
+                g.FillEllipse(b, (int)((myBalls[i].fPos[0]) - ((myBalls[i].fDiam) / 2)), (int)((myBalls[i].fPos[1]) - ((myBalls[i].fDiam) / 2)), (int)myBalls[i].fDiam, (int)myBalls[i].fDiam);
+                //g.FillEllipse(b,)
+            }
+            //g.FillRectangle(b, new Rectangle(0, 0, (int)pb1.Width, (int)mySysVars.iGround));
+              Pen p;
+
+            p = new System.Drawing.Pen(System.Drawing.Color.White);
+            g.DrawLine(p, 0, mySysVars.iHeader, pb1.Width, mySysVars.iHeader);
+        }
+
+        private void tmrUpdate_Tick(object sender, EventArgs e)
+        {
+            tmrUpdate.Enabled = false;
+            for (int i = 0; i < myBalls.Count; i++)
+            {
+                UpdateAcceleration(myBalls[i],myForces[i]);
+                UpdateVelocity(myBalls[i], myControls[i]);
+                UpdatePosition(myBalls[i], myControls[i]);
+                CheckIfPosIsValid(i);
+            }
+            PurgeBalls();
+            pb1.Invalidate();
+            tmrUpdate.Enabled = true;
+        }
+
+        private void tmrMouseCoords_Tick(object sender, EventArgs e)
+        {
+            Point screenCoordinates = this.PointToScreen(new Point(0, 0)); 
+
+            lblMouseX.Text = Convert.ToString(MousePosition.X - screenCoordinates.X);
+            lblMouseY.Text = Convert.ToString(MousePosition.Y - screenCoordinates.Y);
+        }
+
+        // Start Update Ball Movement
+        private void UpdateRandomForce()
+        {
+            Random r = new Random();
+            double d;
+            d = r.Next(-500, 500);
+            mySysVars.fRandForce = d/1000;
+            lblRandX.Text = Convert.ToString(mySysVars.fRandForce);
+        }
+        private void UpdateForces(Force f)
+        {
+            f.fTotal[0] = f.fRand[0]; 
+            f.fTotal[1] = mySysVars.fGravity;
+        }
+        private void UpdateAcceleration(Ball b, Force f)
+        {
+            //a = F/m
+            b.fAcc[0] = f.fTotal[0] / b.fMass;
+            b.fAcc[1] = f.fTotal[1] / b.fMass;
+        }
+        private void UpdateVelocity(Ball b, Control c)
+        {
+            double fmSElapsed;
+
+            TimeSpan dTimeElapsed;
+            if (c.dVLastUpdate != new DateTime())
+            {
+                dTimeElapsed = System.DateTime.Now.Subtract(c.dVLastUpdate);
+                fmSElapsed = dTimeElapsed.Milliseconds + (dTimeElapsed.Seconds * 1000);
+                b.fVel[0] += (fmSElapsed * b.fAcc[0]) / 1000;
+                b.fVel[1] += (fmSElapsed * b.fAcc[1]) / 1000;
+            }
+            c.dVLastUpdate = System.DateTime.Now;
+        }
+        private void UpdatePosition(Ball b, Control c)
+        {
+            double fmSElapsed;
+            TimeSpan dTimeElapsed;
+
+            if (c.dPLastUpdate != new DateTime())
+            {
+                dTimeElapsed = System.DateTime.Now.Subtract(c.dPLastUpdate);
+                fmSElapsed = dTimeElapsed.Milliseconds + (dTimeElapsed.Seconds) * 1000;
+                b.fPos[0] += (fmSElapsed * b.fVel[0]) / 1000;
+                b.fPos[1] += (fmSElapsed * b.fVel[1]) / 1000;
+            }
+            c.dPLastUpdate = System.DateTime.Now;
+        }
+        // End Update Ball Movement
+    }
+}
